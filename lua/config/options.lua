4c63367b015de8395bbd002247522a38f0bc68e4
yosunnyvim <uniscore@proton.me>
2026-06-03 21:33:38 +0300

    edit: scrolloff to 100

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -23,7 +23,7 @@ vim.opt.incsearch = true
 vim.opt.conceallevel = 2
 
 vim.opt.termguicolors = true
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 100
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
